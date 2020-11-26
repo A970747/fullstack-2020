58860691b21eac56b9e9785c26b70a020eb0310d
A970747 <Matt.J.Jackson@gmail.com>
2020-11-26 11:20:28 -0500

    Exercise 2.4, 2.5

--- part2/courseinfo-part2/src/Total.jsx
+++ part2/courseinfo-part2/src/Total.jsx
@@ -4,14 +4,12 @@ const Total = ({course: { parts, ...rest }}) => {
   const [exerciseCount, setExerciseCount] = useState(parts.map(e => e.exercises))
 
   return (
-    <>
-      <p>
-        Number of exercises {' '}
-        {
-        exerciseCount.reduce((a,b) =>  a + b )
-        } 
-      </p>
-    </>
+    <p>
+      Number of exercises {' '}
+      {
+      exerciseCount.reduce((a,b) =>  a + b )
+      } 
+    </p>
   )
 }
 
--- part2/courseinfo-part2/src/index.js
+++ part2/courseinfo-part2/src/index.js
@@ -5,34 +5,52 @@ import './index.css';
 
 
 const App = () => {
-  const [course, setCourse] = useState({
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4
-      }
-    ]
-  });
+  const [course, setCourse] = useState([
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]);
 
-  return  <Course key={course.id} course={course} />
+  return  course.map( course => <Course key={course.id} course={course} />)
 }
 ReactDOM.render(
   <React.StrictMode>
