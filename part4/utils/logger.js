2641384675533907e4a8dccd0a81aef1a81b94d7
a970747 <Matt.J.Jackson@gmail.com>
2020-12-16 12:55:43 -0500

    Exercise 4.1 & 4.2

---
+++ part4/utils/logger.js
@@ -0,0 +1,12 @@
+const info = (...params) => {
+  console.log(...params); // eslint-disable-line
+};
+
+const error = (...params) => {
+  console.error(...params); // eslint-disable-line
+};
+
+module.exports = {
+  info, 
+  error,
+};
---
+++ part4/utils/middleware.js
@@ -0,0 +1,32 @@
+const logger = require('./logger');
+
+const requestLogger = (req, res, next) => {
+  logger.info('Method: ', req.method);
+  logger.info('Path: ', req.path);
+  logger.info('Body: ', req.body);
+  logger.info('---');
+  return next();
+};
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send( { Error: 'Unknown endpoint'} );
+};
+
+const errorHandler = (error, req, res, next) => {
+  logger.error(error._message);
+
+  if (error.name === 'CastError') {
+    return res.status(400).send( {Error: 'Malformatted  ID'} );
+  };
+  if (error.name === 'ValidationError') {
+    return res.status(400).json( {Error: error.message} )
+  };
+
+  return next(error);
+}
+
+module.exports = {
+  requestLogger,
+  unknownEndpoint,
+  errorHandler,
+}
