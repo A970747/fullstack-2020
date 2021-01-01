33b7abf3f9d66d43fb07de6a3907784c83990577
a970747 <Matt.J.Jackson@gmail.com>
2020-12-31 20:05:33 -0500

    Update blog unit test.

--- part5/src/components/blog.unit.test.js
+++ part5/src/components/blog.unit.test.js
@@ -3,29 +3,25 @@ import '@testing-library/jest-dom/extend-expect';
 import {render} from '@testing-library/react';
 import Blog from './Blog';
 
-test('renders content', () => {
-  const blog = {
-    title: 'Test Blog',
-    author: 'Matt Jackson',
-    url: 'www.testingURL.com',
-    likes: 0,
-  };
+describe('testing Blog component', () => {
+  test('renders content', () => {
+    const blog = {
+      title: 'Test Blog',
+      author: 'Matt Jackson',
+      url: 'www.testingURL.com',
+      likes: 0,
+    };
 
-  const component = render(
-    <Blog blog={blog} />,
-  );
+    const component = render(
+      <Blog blog={blog} />,
+    );
 
-  expect(component.container).toHaveTextContent(
-    'Test Blog',
-  );
+    expect(component.container).toHaveTextContent('Test Blog');
 
-  const element = component.getByText(
-    'Test Blog',
-  );
-  expect(element).toBeDefined();
+    const element = component.getByText('Test Blog');
+    expect(element).toBeDefined();
 
-  const div = component.container.querySelector('.blog');
-  expect(div).toHaveTextContent(
-    'Test Blog',
-  );
+    const div = component.container.querySelector('.blog');
+    expect(div).toHaveTextContent('Test Blog');
+  });
 });
